b6e5a9261612df6438a9521b6e3c124adc42db01
Sara Stikic <sarastikic10@gmail.com>
2021-01-28 14:08:45 +0100

    izmena4

--- index.php
+++ index.php
@@ -53,5 +53,43 @@ body {
     /*z-index: 0;*/
 }
 </style>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<div>
+<h1>Oceandrivess.ph</h1>
+<h2>Skola fotografije</h2>
+</div>
+<div id="informacije">
+             <marquee>NA SVAKIH PROVEDENIH 5 ČASOVA <span id="popust">2 PRAKSE GRATIS</span> !</marquee> 
+ </div>  
+ <div id="oNama" >
+             <article id="osnovno2">
+
+                <section id="oNamaNaslov">
+                 <h1>Nešto više o našoj školi</h1>
+                </section>
+
+                <section id="text">
+                  <p>
+                  OceanDrivess.ph je nova škola fotografije koja je krenula sa radom u cilju povećanja
+                  produktivnosti, mašti i proširivanju vidika kod mladih osoba koje vole fotografiju. 
+                  Svaka napravljena slika prirodne može, uz pravilno korišćene parametre koje nam nudi 
+                  fotoaparat, može da proširi vidike kod mladih osoba. 
+                  Pored učlanjenja novih đaka, škola nudi i prijavu fotografa kako bi podelili znanje sa našim učenicima.
+                  Rad sa učenicima se vrši na teritoriji Srbije. Putovanja su sastvani deo časova jer se na taj nači upoznajemo 
+                  sa prirodnim lepotama naše zemlje i trudimo se da zabeležimo svaki momenat.
+                   
+                  <i>Postani i ti deo naše ekipe!</i>                 
+                   </p>     
+                <img data-u="thumb" src="imgs/slika1.png" width="500" height="356" />   
+                <hr>     
+                </section> 
+              </article> 
+          </div>
+          <?php include 'footer.php'; ?>
+
 </body>
 </html>
