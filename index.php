57484486d170f464e655c798e2d1b0c6d0eef68b
Sara Stikic <sarastikic10@gmail.com>
2021-01-28 13:46:05 +0100

    izmena3

--- index.php
+++ index.php
@@ -1,11 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Oceandrivess.ph - Početna</title>
+    
+    <script src="./refresh.js"></script>
+     <script type="text/javascript">
+         refreshContents();
+     </script>
 </head>
 <body>
+<style>
+body {
+  background: linear-gradient(white,gray);
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  padding-bottom: 100px;
+  font-size:15px;
+  text-align: center;
+  margin: 15px 300px 5px 200px;
+}
+#informacije{
+    width: 100%;
+    /*height: 5%;*/
+    line-height: 30px;
+    font-size: 30px;
+    font-weight: bold;
+    font-style: oblique;
+    margin-top: 10%;
+    position: relative;
     
+}
+#osnovno2{
+    width: 100%;
+    float: left;
+    /*margin-top: 170px;*/
+    background: radial-gradient(#d9d9d9,#fff);
+    text-align: center;
+    margin-top: 5%;
+    margin-bottom: 5%;
+    border-radius: 50px;
+    opacity: 0.8; 
+    font-family: 'Cormorant Garamond', serif;
+    font-size: 20px;
+    color: black;
+    z-index: auto;
+    /*z-index: 0;*/
+}
+</style>
 </body>
 </html>
