9c6ca9ed11dca4d3def762ddfb1a05d64baa46b8
Sara Stikic <sarastikic10@gmail.com>
2021-01-28 14:17:40 +0100

    izmena5

---
+++ imgs/footer.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/231963d343.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="footer.css">
+    <title>Footer</title>
+</head>
+<body>
+    
+    <footer id="footer" class="footer">
+        <div class="container text-center">
+      
+        <h3><b>Pratite nas na drustvenim mrezama</b></h3>
+      
+        <ul class="social-links">
+          <li><a href="https://www.facebook.com/Mathiscool5/" target="_blank"><i class="fa fa-facebook fa-fw"></i></a></li>
+          <li><a href="https://www.instagram.com/oceandrivess.ph/" target="_blank"><i class="fa fa-instagram fa-fw"></i></a></li>
+
+        </ul>
+      
+       <i> "Stvarnost je stvarnija ako joj dodaš nestvarnog." #oceandrivess.ph</i>
+        </div>
+      </footer>
+      
+</body>
+</html>
